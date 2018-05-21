db3ed7e87c30ba30b646bb311b329a52fb462def
Phips Peter <pspeter333@gmail.com>
2018-05-21 08:36:07 -0700

    Run fmt
    The imports were not in the right order.

--- src/heading.tsx
+++ src/heading.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
 import Typography from "@material-ui/core/typography";
+import * as React from "react";
 
 export const Heading: React.SFC<{}> = () => {
     return <Typography variant="title">Epidemia</Typography>;
