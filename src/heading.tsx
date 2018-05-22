551a60b41faeb1edc3315ccf77677e39394b8e15
Phips Peter <pspeter3@users.noreply.github.com>
2018-05-21 20:27:52 -0700

    Revert "Add Material UI (#6)" (#8)
    This reverts commit 158731b36f5d97cb3f1cdaf0568396f417ae5df7.

--- src/heading.tsx
+++ src/heading.tsx
@@ -1,6 +1,5 @@
-import Typography from "@material-ui/core/Typography";
 import * as React from "react";
 
 export const Heading: React.SFC<{}> = () => {
-    return <Typography variant="title">Epidemia</Typography>;
+    return <h1>Epidemia</h1>;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,5 @@
-import CssBaseline from "@material-ui/core/CssBaseline";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Heading } from "./heading";
 
-ReactDOM.render(
-    <React.Fragment>
-        <CssBaseline />
-        <Heading />
-    </React.Fragment>,
-    document.getElementById("root"),
-);
+ReactDOM.render(<Heading />, document.getElementById("root"));
