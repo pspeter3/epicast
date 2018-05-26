de32333b888774f08553044dcbb7b7c12c0f3bf5
Phips Peter <pspeter333@gmail.com>
2018-05-25 20:48:29 -0700

    Cowboy Commit
    No risk, no fun.

--- src/theme/data.tsx
+++ src/theme/data.tsx
@@ -8,6 +8,10 @@ export const Meter: React.SFC<{ active?: boolean; value: number }> = ({ active, 
     return <meter className={classNames.join(" ")} value={value} />;
 };
 
+export const Numeric: React.SFC<{ value: number }> = ({ value }) => {
+    return <p className="data-numeric">{value.toFixed(2)}</p>;
+};
+
 export const Percentage: React.SFC<{ value: number }> = ({ value }) => {
     return <p className="data-percentage">{Math.round(value * 100)}%</p>;
 };
