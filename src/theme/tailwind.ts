ce6272a7b7099e06c86f319637e1f66a8965e039
Phips Peter <pspeter333@gmail.com>
2018-08-07 21:50:08 -0700

    Add Tailwind enums
    Add initial Tailwind CSS.

---
+++ src/theme/tailwind.ts
@@ -0,0 +1,55 @@
+export const enum Position {
+    Static = "static",
+    Fixed = "fixed",
+    Absolute = "absolute",
+    Relative = "relative",
+    Sticky = "sticky",
+}
+
+export const enum Pin {
+    Top = "pin-t",
+    Right = "pin-r",
+    Bottom = "pin-b",
+    Left = "pin-l",
+    Vertical = "pin-y",
+    Horizontal = "pin-x",
+    All = "pin",
+    None = "pin-none",
+}
+
+export const enum Display {
+    Block = "block",
+    InlineBlock = "inline-block",
+    Inline = "inline",
+    Table = "table",
+    TableRow = "table-row",
+    TableCell = "table-cell",
+    Hidden = "hidden",
+    Flex = "flex",
+    InlineFlex = "inline-flex",
+}
+
+export const enum AlignItems {
+    Stretch = "items-stretch",
+    Start = "items-start",
+    Center = "items-center",
+    End = "items-end",
+    Baseline = "items-baseline",
+}
+
+export const enum JustifyContent {
+    Start = "justify-start",
+    Center = "justify-center",
+    End = "justify-end",
+    Between = "justify-between",
+    Around = "justify-around",
+}
+
+export const enum BoxShadow {
+    Shadow = "shadow",
+    Medium = "shadow-md",
+    Large = "shadow-lg",
+    Inner = "shadow-inner",
+    Outline = "shadow-outline",
+    None = "shadow-none",
+}
