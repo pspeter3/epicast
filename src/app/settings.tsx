09ce697ba7af0d1085acea6cd77452e95bc94608
Phips Peter <pspeter333@gmail.com>
2018-08-23 14:32:01 -0400

    Add Form elements

--- src/app/settings.tsx
+++ src/app/settings.tsx
@@ -2,28 +2,9 @@ import * as React from "react";
 import { StandardConfig } from "../core/configs";
 import { Config } from "../core/types";
 import { ActionProps, Appbar } from "../theme/appbar";
-import { classNames, focusClass } from "../theme/css";
+import { Button, Fieldset, Legend } from "../theme/form";
 import { FormField } from "../theme/form_field";
 import { BackIcon, CodeIcon, SaveIcon } from "../theme/icons";
-import {
-    AlignItems,
-    BackgroundColor,
-    BorderColor,
-    BorderRadius,
-    BorderSize,
-    BorderStyle,
-    BoxShadow,
-    Display,
-    FontWeight,
-    JustifyContent,
-    Outline,
-    Padding,
-    Sizing,
-    TextColor,
-    TextDecoration,
-    TextSize,
-    Tracking,
-} from "../theme/tailwind";
 import { DialogService } from "../util/services";
 import { difference, unit } from "../util/stacks";
 import { CityRow } from "./city_row";
@@ -39,49 +20,6 @@ export interface Props {
 
 export class Settings extends React.PureComponent<Props, Config> {
     public static displayName = "Settings";
-
-    public static Legend: React.SFC<React.HTMLProps<HTMLLegendElement>> = props => (
-        <legend
-            {...props}
-            className={classNames(
-                FontWeight.Medium,
-                Padding.A3,
-                Sizing.H12,
-                TextColor.Grey,
-                TextSize.Small,
-                Tracking.Wide,
-                props.className,
-            )}
-        />
-    );
-
-    public static ButtonField: React.SFC<React.HTMLProps<HTMLButtonElement>> = props => (
-        <div className={classNames(Padding.X3, Padding.Y1)}>
-            <button
-                {...props}
-                className={classNames(
-                    AlignItems.Center,
-                    BorderColor.Base,
-                    BorderRadius.Small,
-                    BorderSize.A2,
-                    BorderStyle.Solid,
-                    Display.InlineFlex,
-                    FontWeight.Medium,
-                    JustifyContent.Center,
-                    Padding.X4,
-                    Sizing.H10,
-                    Sizing.WFull,
-                    TextColor.Base,
-                    TextDecoration.Uppercase,
-                    TextSize.Small,
-                    focusClass(BackgroundColor.Lightest),
-                    focusClass(BoxShadow.Large),
-                    focusClass(Outline.None),
-                    props.className,
-                )}
-            />
-        </div>
-    );
 
     public state = this.props.config;
 
@@ -118,11 +56,9 @@ export class Settings extends React.PureComponent<Props, Config> {
                         value={this.state.epidemics}
                         onChange={this._onChangeEpidemics}
                     />
-                    <Settings.ButtonField onClick={this._onStandardConfig}>
-                        Standard Configuration
-                    </Settings.ButtonField>
-                    <fieldset>
-                        <Settings.Legend>Cities</Settings.Legend>
+                    <Button onClick={this._onStandardConfig}>Standard Configuration</Button>
+                    <Fieldset>
+                        <Legend>Cities</Legend>
                         {Object.keys(this.state.cities).map(city => (
                             <CityRow
                                 key={city}
@@ -131,10 +67,10 @@ export class Settings extends React.PureComponent<Props, Config> {
                                 onChange={this._onChange}
                             />
                         ))}
-                        <Settings.ButtonField onClick={this._onAddCity}>
+                        <Button onClick={this._onAddCity}>
                             Add City
-                        </Settings.ButtonField>
-                    </fieldset>
+                        </Button>
+                    </Fieldset>
                 </>
             </>
         );
@@ -185,6 +121,3 @@ export class Settings extends React.PureComponent<Props, Config> {
         });
     };
 }
-
-Settings.Legend.displayName = "Settings.Legend";
-Settings.ButtonField.displayName = "Settings.ButtonField";
