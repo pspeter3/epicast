fdea22d9a569c2fbc6397e1d5713575338dad614
Phips Peter <pspeter333@gmail.com>
2018-08-23 11:16:29 -0400

    Convert Picker
    Conver the picker component.

--- src/theme/picker.tsx
+++ src/theme/picker.tsx
@@ -1,23 +1,5 @@
 import * as React from "react";
-import { classNames, focusClass } from "./css";
-import {
-    AlignItems,
-    Appearance,
-    BackgroundColor,
-    BorderRadius,
-    BorderSize,
-    BorderStyle,
-    Display,
-    FontWeight,
-    JustifyContent,
-    Outline,
-    Padding,
-    Sizing,
-    TextColor,
-    TextDecoration,
-    TextSize,
-    Tracking,
-} from "./tailwind";
+import { classNames } from "./css";
 
 export interface Props {
     icon: React.ReactElement<any>;
@@ -32,67 +14,25 @@ export class Picker extends React.PureComponent<Props, {}> {
     public static displayName = "Picker";
 
     public static Field: React.SFC<React.HTMLProps<HTMLDivElement>> = props => (
-        <div
-            {...props}
-            className={classNames(Display.Flex, Padding.X3, Padding.Y1, props.className)}
-        />
+        <div {...props} className={classNames("picker", props.className)} />
     );
 
     public static Prefix: React.SFC<React.HTMLProps<HTMLDivElement>> = props => (
-        <div
-            {...props}
-            className={classNames(
-                AlignItems.Center,
-                BorderRadius.SmallLeft,
-                BorderSize.B1,
-                BorderSize.L1,
-                BorderSize.T1,
-                Display.Flex,
-                JustifyContent.Center,
-                Sizing.H10,
-                Sizing.W10,
-                TextColor.Grey,
-                props.className,
-            )}
-        />
+        <div {...props} className={classNames("picker__prefix", props.className)} />
     );
 
     public static Dropdown: React.SFC<React.HTMLProps<HTMLSelectElement>> = props => (
-        <select
-            {...props}
-            className={classNames(
-                Appearance.None,
-                BorderRadius.None,
-                BorderRadius.SmallRight,
-                BorderSize.A1,
-                BorderStyle.Solid,
-                FontWeight.Medium,
-                Padding.X2,
-                Sizing.H10,
-                Sizing.WFull,
-                TextColor.Grey,
-                TextDecoration.Uppercase,
-                TextSize.Base,
-                Tracking.Wide,
-                focusClass(BackgroundColor.Lightest),
-                focusClass(Outline.None),
-                props.className,
-            )}
-        />
+        <select {...props} className={classNames("picker__dropdown", props.className)} />
     );
 
     public render() {
         const { icon, label, options, disabled, className } = this.props;
-        const bg = disabled === true ? BackgroundColor.Disabled : BackgroundColor.White;
         return (
             <Picker.Field className={className}>
-                <Picker.Prefix className={bg}>{icon}</Picker.Prefix>
-                <Picker.Dropdown
-                    className={bg}
-                    value={label}
-                    disabled={disabled}
-                    onChange={this._onChange}
-                >
+                <Picker.Prefix className={disabled ? "picker__prefix--disabled" : ""}>
+                    {icon}
+                </Picker.Prefix>
+                <Picker.Dropdown value={label} disabled={disabled} onChange={this._onChange}>
                     <option disabled={true} value={label}>
                         {label}
                     </option>
